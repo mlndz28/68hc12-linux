30933e8f804999d090543c7dcba2e845c85ed10e
Fabian Melendez <fabian.melendez.a@gmail.com>
2019-09-21 13:47:01 -0600

    Changed installation path for binaries

--- installer.sh
+++ installer.sh
@@ -9,11 +9,11 @@ mkdir -p /tmp/hc12asm
 wget -q https://hcs12text.com/files/asmide340.zip -O /tmp/hc12asm/asmide.zip --show-progress
 busybox unzip /tmp/hc12asm/asmide.zip -o -d /tmp/hc12asm/
 if [ -f assembler.sh ]; then
-    cp assembler.sh /bin/as12
+    cp assembler.sh /usr/bin/as12
 else
-    wget -q https://raw.githubusercontent.com/mlndz28/68hc12-linux/master/assembler.sh -O /bin/as12 --show-progress
+    wget -q https://raw.githubusercontent.com/mlndz28/68hc12-linux/master/assembler.sh -O /usr/bin/as12 --show-progress
 fi
-chmod 755 /bin/as12
+chmod 755 /usr/bin/as12
 mkdir -p /opt/hc12asm
 cp /tmp/hc12asm/as12.exe /opt/hc12asm
 echo -e "\n  Done.\n"
